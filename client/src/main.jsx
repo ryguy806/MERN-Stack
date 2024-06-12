e65ced3b863ef56508a488eef13d72b185dbd3e3
Ryan Guelzo <ryan.t.guelzo.ctr@health.mil>
2024-06-12 10:27:15 -0700

    RTK all set up.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,15 @@ import "./index.css";
 import { configureStore } from "@reduxjs/toolkit";
 import globalReducer from "./state/index";
 import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "./state/api";
 
 const store = configureStore({
-  reducer: { global: globalReducer },
+  reducer: { global: globalReducer, [api.reducerPath]: api.reducerPath },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
 });
+
+setupListeners(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
