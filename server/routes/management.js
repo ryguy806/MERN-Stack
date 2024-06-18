3bbc6844d7b36b00d1e3b1248d12aa1e58308473
Ryan Guelzo <ryan.t.guelzo.ctr@health.mil>
2024-06-18 09:02:05 -0700

    Set up admin on the server.

--- server/routes/management.js
+++ server/routes/management.js
@@ -1,5 +1,7 @@
 import express from "express";
+import { getAdmins } from "../controllers/management.js";
 
 const router = express.Router();
+router.get("/admin", getAdmins);
 
 export default router;
